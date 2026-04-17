3cec0becc3d3f9cd779f3a1dc0588b7415892492
Davod Dice <david.c4f@hotmail.com>
2026-04-17 09:45:38 +0700

    Add PI Alphabit

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae7e2145b6056d6a31522eac1ab70e151b04079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302d8b5607fde7b0d56ad57ec0fe73e73b1c63f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
