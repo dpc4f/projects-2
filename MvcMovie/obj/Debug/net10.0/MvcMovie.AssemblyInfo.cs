e401347342884bc5ff44ce04c6767e90389442be
Davod Dice <david.c4f@hotmail.com>
2026-04-20 02:58:04 +0700

    add 100 px margin-top

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302d8b5607fde7b0d56ad57ec0fe73e73b1c63f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ca9cd5e0ccd4038c0315f41c039697ca08b021")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
