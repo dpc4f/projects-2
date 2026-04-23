5f47d3d409022bd57f3f16187995804a59032d9b
Davod Dice <david.c4f@hotmail.com>
2026-04-23 16:36:37 +0700

    update applications' title

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5948e9559c1e5a2ded4c04da9dc2503e89670668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce12b69ef4f6dbde9412d2feb089f02d7c5261fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
