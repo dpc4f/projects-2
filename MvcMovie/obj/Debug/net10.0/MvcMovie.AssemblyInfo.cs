b94f98a40fd5311b9668240eb38cf8bb9e678786
Davod Dice <david.c4f@hotmail.com>
2026-04-23 19:22:08 +0700

    correct ð function, 'convertToElte()'; update GUI

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f47d3d409022bd57f3f16187995804a59032d9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21519099666b4e8e295e6090cce1bb65b6ddcae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
