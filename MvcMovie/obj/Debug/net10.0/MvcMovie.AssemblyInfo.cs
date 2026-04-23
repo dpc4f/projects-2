65b35b7dee320e1b9cac2b13520f4b57b4033c74
Davod Dice <david.c4f@hotmail.com>
2026-04-23 11:11:47 +0700

    add an application to map beloved wives 2 favourite animals

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad46204381f4cb7b1bba58e712cf96a234f7f15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18af7c04f6b532e3f7099204bf66b81d1ff6bff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
