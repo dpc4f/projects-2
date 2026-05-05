87cb8485acffb8834a4bd5f876b12b358fc6950c
Davod Dice <david.c4f@hotmail.com>
2026-05-05 07:34:19 +0700

    add folder 02; 03 into web.dev/Flexbox

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c685d2f5bb40346047a318068ec65c427a3f1bf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f332498918d4fe71ee518d5d81cf8af1befe4d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
