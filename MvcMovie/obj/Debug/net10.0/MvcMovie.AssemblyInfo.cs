cd645ad579d50c2e7e0ce10f10358d101b835f8c
Davod Dice <david.c4f@hotmail.com>
2026-05-05 19:50:08 +0700

    replace 'Programmer' by, 'Developer'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f332498918d4fe71ee518d5d81cf8af1befe4d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87cb8485acffb8834a4bd5f876b12b358fc6950c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
