e6c3c3328f876eb78091b2dde307fe7e88d2b18d
Davod Dice <david.c4f@hotmail.com>
2026-05-07 12:03:27 +0700

    differentiate between, 'as known as' && 'hiệu là'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e10edb3c328795e3ffab0fff89acd355e527473b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bf92a5390e63cb042c57376a00e89bc2292493")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
