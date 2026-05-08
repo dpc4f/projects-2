39c666ea759fbcf77a188732ca570a00bf650f9a
Davod Dice <david.c4f@hotmail.com>
2026-05-08 08:00:31 +0700

    add a rule for names of Davie

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bf92a5390e63cb042c57376a00e89bc2292493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03761b50a7a291bc6ddd08a20384f75bd1d58469")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
