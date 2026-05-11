36851e841b69c97540c061d8ac1ae9b7491e6a79
Davod Dice <david.c4f@hotmail.com>
2026-05-11 08:28:08 +0700

    revert back to use previous notation for hours

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b39137a034b7c0731ed87279d7d101714f112f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2c9fef96b62e4a819577ba88b5a3414c7f0b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
