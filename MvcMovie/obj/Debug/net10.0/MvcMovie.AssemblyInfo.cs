33449aa52c78273ae2455b419c476710db2832ee
Davod Dice <david.c4f@hotmail.com>
2026-05-12 09:46:34 +0700

    modify a name in the list of names of Dijkstra Python

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2c9fef96b62e4a819577ba88b5a3414c7f0b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd2ae31fcb7f073d018f3ef622a83a9f71ded76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
