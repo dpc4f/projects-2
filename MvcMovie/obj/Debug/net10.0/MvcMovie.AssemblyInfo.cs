5f487f775498206629deec76dd8f969d41355185
Davod Dice <david.c4f@hotmail.com>
2026-05-14 05:48:16 +0700

    beautify the *.html, 'Transformation Between Lure 'N' Rule'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eb9a8343a71b56322ebd56fcf4dbb467d940a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19fec9491028742b6c33b2ba1e2649ada91815e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
