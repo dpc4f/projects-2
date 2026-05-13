32e5c067cda157fe2c0b80cade3112cddd68283a
Davod Dice <david.c4f@hotmail.com>
2026-05-13 19:05:29 +0700

    welcome Princess Sofa de Todos los Santos de Borbn y Ortiz join Davios' family .! <3 :-)

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7179e5353eba1e20f7f2c9860894f431dcb9babe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eb9a8343a71b56322ebd56fcf4dbb467d940a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
