5806408dfed2d3444c1a2f2a28ac61a4b6398b7e
Davod Dice <david.c4f@hotmail.com>
2026-05-20 09:42:00 +0700

    add 'CSS class .ambitious 4 letter, 'A a'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f12c8f79241498882e7d24b7971b811ffa21ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e11c1d1529ace1cfbbd944b35ec529be2485bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
