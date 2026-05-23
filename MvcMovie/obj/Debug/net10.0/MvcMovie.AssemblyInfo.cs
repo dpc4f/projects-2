030492c79cf69478f94751046b4a32e9255e470e
Davod Dice <david.c4f@hotmail.com>
2026-05-23 12:54:29 +0700

    change colours of application, 'PI Alphabet'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f240a01e79dd6863e301db63670d1c7a8afda5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bee4b4f7494443a6a613ec18ddc5df080341023")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
