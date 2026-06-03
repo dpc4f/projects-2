86bc0a295a4d7cc9a801a5214dbbf818eef46ae4
Davod Dice <david.c4f@hotmail.com>
2026-06-03 14:43:49 +0700

    del Miao

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386a58c83aa80ea3104b062c826b230ae09811a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba776d5a2e59efe63217c3d973629ff832bf178c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386a58c83aa80ea3104b062c826b230ae09811a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba776d5a2e59efe63217c3d973629ff832bf178c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
