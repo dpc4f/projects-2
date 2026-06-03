ba776d5a2e59efe63217c3d973629ff832bf178c
Davod Dice <david.c4f@hotmail.com>
2026-06-03 11:21:18 +0700

    minor update(s)

--- TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5b0c3a287f895f74f09956920d6721fefed0d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386a58c83aa80ea3104b062c826b230ae09811a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
