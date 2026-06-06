01d2a072cba3fb66ef1f7264fa0ecfb62f999ffb
Davod Dice <david.c4f@hotmail.com>
2026-06-06 16:28:39 +0700

    update colours 4 months in the application, 'Elte Calendar && Platon Time'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ea2948ec7c3a99fddee3c1d63c4469c3455740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568423a27ba3abd9818e5cb26f4e5948f0596324")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
