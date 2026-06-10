93e0e5b60e6506b7344d9ff3c5ab25b96eedc1bc
Davod Dice <david.c4f@hotmail.com>
2026-06-10 10:24:26 +0700

    initial version of ð application, 'Track Of Chess Result'

--- TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568423a27ba3abd9818e5cb26f4e5948f0596324")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf46562f14e49c7c5fe972164eb1c82a28b362c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
