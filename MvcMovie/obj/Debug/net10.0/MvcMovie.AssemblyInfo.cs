9c256fe1fccfcb914de419058a32f70998ea5e00
Davod Dice <david.c4f@hotmail.com>
2026-06-13 14:01:04 +0700

    tweak the application, 'Transformation Between Yan Annabel 'N' Sofia De Borbon'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8534a4f0edcee864aeaa58712f0f6a9122032667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83243a3cffcb187926ba03135147932d5bce0601")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
