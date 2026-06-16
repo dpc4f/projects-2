e01ff8df37ca055d2da59701ad8689f00447bda4
Davod Dice <david.c4f@hotmail.com>
2026-06-16 09:19:20 +0700

    update Adverb to become Adjective 'cause of standing b4 a Noun in name of an application

--- TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c256fe1fccfcb914de419058a32f70998ea5e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e028055e556683be8080e2df51817de5eaf722a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
