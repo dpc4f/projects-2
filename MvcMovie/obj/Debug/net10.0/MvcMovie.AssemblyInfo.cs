8d929af2e54bd4e7b81aac7e35d2792c73305d4f
Davod Dice <david.c4f@hotmail.com>
2026-06-19 18:03:46 +0700

    update font-size of, 'CSS class .excellent-def .excellent'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee3a6c18a3569512edd4f3fd28b04337f782ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496670237f897c0b282772454893494baebdcf44")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
