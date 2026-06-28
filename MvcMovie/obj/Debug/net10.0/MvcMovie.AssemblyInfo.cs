2cf98c0505b12cf4ecce480b0ba3b5d63ca4b4cb
Davod Dice <david.c4f@hotmail.com>
2026-06-29 03:47:11 +0700

    modify the application, 'Transfrom Between Yao Annabel && Infanta Sofia - 2'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd12d57191b95f21c3c246cae099371dadaf8ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0314f864fdc96e61350f7ab59b45839f865e2b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
