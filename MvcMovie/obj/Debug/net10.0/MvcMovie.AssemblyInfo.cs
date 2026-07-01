d7d66a397abcf3b2fa01ab6ff30f5339b36ce698
Davod Dice <david.c4f@hotmail.com>
2026-07-02 06:30:27 +0700

    update names of Dustin's

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502ef0ed3ccbcbfcf0f2065ff9277fdbce58d2e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea2411a68527b88ea24814a8c0ea7bf9f276315")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
