15237e22017c2d5a179800460c9a52d8840adf4c
Davod Dice <david.c4f@hotmail.com>
2026-07-02 10:27:00 +0700

    update Elte Calendar w/ Platon Time; 'Formula Of Beloved Women 'N' ...'

--- TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ TestClassLib/ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea2411a68527b88ea24814a8c0ea7bf9f276315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d66a397abcf3b2fa01ab6ff30f5339b36ce698")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
