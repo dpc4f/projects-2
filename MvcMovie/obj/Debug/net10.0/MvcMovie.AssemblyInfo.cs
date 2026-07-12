c07c6b052190410cc34b8a2d457ec4d0f4a9234b
Davod Dice <david.c4f@hotmail.com>
2026-07-12 13:46:03 +0700

    add another path 4, 'rule 24'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4adc3f6ddd3f6daf6a461a9b1d38f7a2a3086d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0d841dd117c4b06a4d67465bb43eea2028a916")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
