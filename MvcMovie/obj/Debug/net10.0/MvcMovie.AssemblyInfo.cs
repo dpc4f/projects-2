91c785677910cfae7b6469eca3bd939e7724f366
Davod Dice <david.c4f@hotmail.com>
2026-07-13 16:02:13 +0700

    update the app, 'Triangle Of DIY'

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c20af5d4945eb53099790876ed511adbeb1bbc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c177a236f487a49bf7f831aece922fc437f82675")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
