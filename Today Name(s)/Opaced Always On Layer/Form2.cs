3a6d4a4f371e7f1f3329fc4890a385bd86bd508d
Davod Dice <david.c4f@hotmail.com>
2026-07-21 18:42:08 +0700

    minor update(s)

--- Today Name(s)/Opaced Always On Layer/Form2.cs
+++ Today Name(s)/Opaced Always On Layer/Form2.cs
@@ -15,7 +15,7 @@ namespace Opaced_Always_On_Layer
         // Define the Windows API Constants
         private const int WS_EX_TRANSPARENT = 0x20;
         private const int WS_EX_LAYERED = 0x80000;
-        Color color = Color.LightGreen;
+        Color color = Color.DarkBlue;
 
         public Form2()
         {
