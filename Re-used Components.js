219381f9d2f00993e5b65f1f382ca0ead3c62dba
Davod Dice <david.c4f@hotmail.com>
2026-08-21 01:36:26 +0700

    major update on Date w/ Time in application(s)

--- Re-used Components.js
+++ Re-used Components.js
@@ -29,19 +29,19 @@ function getMonth(month, bLongForm = true) {
             break;
 
         case '6':
-            ret = bLongForm ? 'SEPT' : 's';
+            ret = bLongForm ? 'SEPT' : 'S';
             break;
 
         case '7':
-            ret = bLongForm ? 'OCT' : 'o';
+            ret = bLongForm ? 'OCT' : 'O';
             break;
 
         case '8':
-            ret = bLongForm ? 'NOV' : 'n';
+            ret = bLongForm ? 'NOV' : 'N';
             break;
 
         case '9':
-            ret = bLongForm ? 'DEC' : 'd';
+            ret = bLongForm ? 'DEC' : 'D';
             break;
 
         case '10':
@@ -176,4 +176,133 @@ function convertToElteWithDayName(bShortForm = false, aDate = '') {
     let dayNameInAWeek = getDay((TODAY.getDay() + 5) % 7, !bShortForm);
 
     return elteDOB + ' ' + dayNameInAWeek;    
+}
+
+function getIndexFromElteMonth(month) {
+    let ret = -1;
+
+    switch (month) {
+        case 'Athen':
+        case 'a':
+            ret = 0;
+            break;
+
+        case 'Duo':
+        case 'd':
+            ret = 1;
+            break;
+
+        case 'Magha':
+        case 'm':
+            ret = 2;
+            break;
+
+        case 'Qaru':
+        case 'q':
+            ret = 3;
+            break;
+
+        case 'Felle':
+        case 'f':
+            ret = 4;
+            break;
+
+        case 'Yune':
+        case 'y':
+            ret = 5;
+            break;
+
+        case 'SEPT':
+        case 'S':
+            ret = 6;
+            break;
+
+        case 'OCT':
+        case 'O':
+            ret = 7;
+            break;
+
+        case 'NOV':
+        case 'N':
+            ret = 8;
+            break;
+
+        case 'DEC':
+        case 'D':
+            ret = 9;
+            break;
+
+        case 'Palm':
+        case 'p':
+            ret = 10;
+            break;
+
+        case 'Hose':
+        case 'h':
+            ret = 11;
+            break;
+
+        default:
+            break;
+    }
+
+    return ret;
+}
+
+const MONTH_NAMES = [ // original months' name
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+function getAnimal(idx) {
+    let ret = '';
+
+    switch (idx) 
+    {
+        case 0:
+            ret = "Mouse / Rat / Capybara";
+            break;
+        case 1:
+            ret = "Ox / Bull / Buffalo / Cow";
+            break;
+        case 2:
+            ret = "Tiger / Leopard";
+            break;
+        case 3:
+            ret = "Rabbit / Rapoo ð Computer Mouse";
+            break;
+        case 4:
+            ret = "Long-dragon / Ant / Bee / Peacock";
+            break;
+        case 5:
+            ret = "Snake Clan / Centipede";
+            break;
+        case 6:
+            ret = "Horse / Sea-horse / Dragon";
+            break;
+        case 7:
+            ret = "ma-Goat / Gorilla";
+            break;
+        case 8:
+            ret = "Monkey / Ape";
+            break;
+        case 9:
+            ret = "Rooster / Hen";
+            break;
+        case 10:
+            ret = "Dodge / Deer / Reindeer / Duck";
+            break;
+        case 11:
+            break;
+            ret = "Pig / Boar in ð Wilderness / Bear";
+        default:
+            break;
+    }
+
+    return ret;
+}
+
+function convertReversedCurrentYear(yearStr) {
+    const match = yearStr.match(/^current_year([+-]\d+)$/);
+    return CURRENT_YEAR + Number(match?.[1] ?? 0);
 }
