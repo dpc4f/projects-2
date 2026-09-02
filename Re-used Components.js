8ad41440ad3a24665859c89853b0c13db10ad789
Davod Dice <david.c4f@hotmail.com>
2026-09-03 06:34:40 +0700

    revert back to previous verions of, 'Re-used Components'

--- Re-used Components.js
+++ Re-used Components.js
@@ -610,11 +610,12 @@ class TimeValues {
         this.m = this.Today.getMinutes();
         this.s = this.Today.getSeconds();
         this.ms = this.Today.getMilliseconds();
+        this.constants = new TimeValues.Constants();
         
         this.callback = null;
         this.idcb = null;
 
-        setInterval(() => this.timeTheCurrent(), TimeValues.Constants.TWO_HOURS_IN_MILLISECONDS);
+        setInterval(() => this.timeTheCurrent(), this.constants.TWO_HOURS_IN_MILLISECONDS);
     }
 
     timeTheCurrent() {
@@ -690,15 +691,15 @@ class TimeValues {
     }
 
     updateTimeWithIntervalOneSecond(callback) { // for time value; in HH:MM
-        let fractionOfASecond = ((new Date()).getMilliseconds()) % TimeValues.Constants.ONE_SECOND_IN_MILLISECONDS;
-        let duration = TimeValues.Constants.ONE_SECOND_IN_MILLISECONDS - fractionOfASecond; // will be elapsed duration in milliseconds
+        let fractionOfASecond = ((new Date()).getMilliseconds()) % this.constants.ONE_SECOND_IN_MILLISECONDS;
+        let duration = this.constants.ONE_SECOND_IN_MILLISECONDS - fractionOfASecond; // will be elapsed duration in milliseconds
         this.callback = callback;
         
         if (this.idcb) 
             clearInterval(this.idcb);
 
         setTimeout(() => { 
-            this.idcb = setInterval(() => this.increaseSecond(), TimeValues.Constants.ONE_SECOND_IN_MILLISECONDS);
+            this.idcb = setInterval(() => this.increaseSecond(), this.constants.ONE_SECOND_IN_MILLISECONDS);
             this.increaseSecond();
         }, duration);
     }
@@ -709,8 +710,8 @@ class TimeValues {
         /// --> done
 
         const StartTime = Date.now();
-        const FractionOfAMinuteInMillisecond = StartTime % TimeValues.Constants.ONE_MINUTE_IN_MILLISECONDS;
-        let duration = TimeValues.Constants.ONE_MINUTE_IN_MILLISECONDS - FractionOfAMinuteInMillisecond; // elapsed time in milliseconds
+        const FractionOfAMinuteInMillisecond = StartTime % this.constants.ONE_MINUTE_IN_MILLISECONDS;
+        let duration = this.constants.ONE_MINUTE_IN_MILLISECONDS - FractionOfAMinuteInMillisecond; // elapsed time in milliseconds
         // console.log(duration);
         
         this.callback = callback;
@@ -721,7 +722,7 @@ class TimeValues {
         setTimeout(() => {
             this.s = 0;
             this.increaseMinute(); // doesn't cost much
-            this.idcb = setInterval(() => this.increaseMinute(), TimeValues.Constants.ONE_MINUTE_IN_MILLISECONDS);
+            this.idcb = setInterval(() => this.increaseMinute(), this.constants.ONE_MINUTE_IN_MILLISECONDS);
         }, duration);
     }
 }
@@ -735,19 +736,19 @@ TimeValues.Constants = class {
         this.__ONE_MINUTE_IN_MILLISECONDS = 60000;
     }
     
-    static get ONE_SECOND_IN_MILLISECONDS() {
+    get ONE_SECOND_IN_MILLISECONDS() {
         return this.__ONE_SECOND_IN_MILLISECONDS;
     }
 
-    static get TWO_HOURS_IN_MILLISECONDS() {
+    get TWO_HOURS_IN_MILLISECONDS() {
         return this.__1_CANH_GIỜ_TÍNH_THEO_MIÊU_LY_GIÂY;
     }
 
-    static get ONE_MINUTE_IN_SECONDS() {
+    get ONE_MINUTE_IN_SECONDS() {
         return this.__ONE_MINUTE_IN_SECONDS;
     }
 
-    static get ONE_MINUTE_IN_MILLISECONDS() {
+    get ONE_MINUTE_IN_MILLISECONDS() {
         return this.__ONE_MINUTE_IN_MILLISECONDS;
     }
 }
