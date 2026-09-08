4c8dd6f40c44dc3eba99ff14d7f82f99797a12b9
Davod Dice <david.c4f@hotmail.com>
2026-09-08 20:48:54 +0700

    minor update(s)

--- Re-used Components.js
+++ Re-used Components.js
@@ -1080,7 +1080,7 @@ DateValues.Constants = class {
 function exchange2AnimalByLunar(birthYear) {
     let tmp = Math.abs(CURRENT_YEAR - birthYear) % 12;
     
-    if (birthYear < CURRENT_YEAR) 
+    if (birthYear < CURRENT_YEAR)
         return ANIMAL_NAMES[12 - tmp];
     else
         return ANIMAL_NAMES[tmp];
