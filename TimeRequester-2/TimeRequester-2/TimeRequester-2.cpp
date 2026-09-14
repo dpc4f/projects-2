f3783a45d643e1d122a186d89ffea79a0923fedf
Davod Dice <david.c4f@hotmail.com>
2026-09-15 02:46:48 +0700

    minor update(s)

--- TimeRequester-2/TimeRequester-2/TimeRequester-2.cpp
+++ TimeRequester-2/TimeRequester-2/TimeRequester-2.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-
 #include "TimeRequester-2.h"
 
 using namespace System;
@@ -7,17 +6,12 @@ using namespace System::Net;
 using namespace System::Net::Sockets;
 using namespace TimeRequester2;
 
-
 System::DateTime TimeRequester::GetNetworkTime()
 {
 	// Use the closest regional pool or Cloudflare
 	String^ ntpServer = "time.windows.com";
-
-	//// NTP message size is 48 bytes
-	array<Byte>^ ntpData = gcnew array<Byte>(48);
-
-	//// Setting Leap Indicator, Version Number, and Mode (Client = 3)
-	ntpData[0] = 0x1B;
+	array<Byte>^ ntpData = gcnew array<Byte>(48); // NTP message size is 48 bytes
+	ntpData[0] = 0x1B; // Setting Leap Indicator, Version Number, and Mode (Client = 3)
 
 	array<System::Net::IPAddress^>^ addresses = Dns::GetHostAddresses(ntpServer);
 	IPEndPoint^ ipEndPoint = gcnew IPEndPoint(addresses[0], 123);
@@ -26,13 +20,10 @@ System::DateTime TimeRequester::GetNetworkTime()
 	Exception ex;
 
 	try {
-		//using (String^ tmp = gcnew String("abcd"))
-		//{
 		socket->Connect(ipEndPoint);
 		socket->ReceiveTimeout = 3000; // 3 seconds timeout
 		socket->Send(ntpData);
 		socket->Receive(ntpData);
-		/*}*/
 
 		//// Transmit Timestamp starts at byte 40 (Seconds and Fractions)
 		UInt64 intPart = BitConverter::ToUInt32(ntpData, 40);
@@ -45,13 +36,15 @@ System::DateTime TimeRequester::GetNetworkTime()
 		UInt64 milliseconds = (intPart * 1000L) + ((fractPart * 1000L) / 0x100000000L);
 
 		//// NTP time starts on Jan 1, 1900
-		networkDateTime = (gcnew DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind::Utc))
-			->AddMilliseconds((UInt64)milliseconds);
+		networkDateTime = 
+			(gcnew DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind::Utc))->AddMilliseconds((UInt64)milliseconds);
 	}
-	catch (Exception^ ex) {
+	catch (Exception^ ex)
+	{
 
 	}
-	finally{
+	finally
+	{
 		delete socket;
 	}
 
